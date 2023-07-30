9788a0eec5756aeb2575964727d5643e4ab19bb3
Farhaan <mohammedfarhaan15336@gmail.com>
2023-07-30 13:40:23 +0530

    added env

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
@@ -19,6 +20,6 @@ app.post("/api", async (req, res) => {
 
   res.send(data);
 });
-app.listen(5000, () => {
-  console.log("listening on port 5000");
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
